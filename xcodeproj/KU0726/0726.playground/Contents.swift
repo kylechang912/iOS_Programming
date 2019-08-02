6ffd9be69aa1ea613d5c7686480a4efe7da4865b
Chang Chan Woo <48425270+kylechang912@users.noreply.github.com>
2019-08-02 16:19:22 +0900

    Add files via upload

---
+++ xcodeproj/KU0726/0726.playground/Contents.swift
@@ -0,0 +1,91 @@
+import UIKit
+
+/*
+class Tiger {
+    //Tiger 공장
+    //init = initializer
+    init() {
+    }
+}
+
+let kitty = Tiger.init()
+let tigger = Tiger.init()
+let korea = Tiger.init()
+
+class Eagle {
+    
+    var age = 10
+    var name = "연세"
+    
+    func fly() {
+        print("날아")
+    }
+
+    func sing() {
+        print("노래불러")
+    }
+
+    func hunt() {
+        print("사냥")
+    }
+    
+    init() {
+    }
+}
+
+let chicken = Eagle.init()
+chicken.fly()
+chicken.name = "연세치킨"
+chicken.age = 50
+chicken.sing()
+chicken.hunt()
+let chamsae = Eagle.init()
+let yonsei = Eagle.init()
+
+
+class Person {
+    
+    var kind = "인류"
+    
+    func eat() {
+        print("먹는다")
+    }
+    
+    func hear() {
+        print("듣는다")
+    }
+    
+    func feel() {
+        print("느끼다")
+    }
+    
+    init() {
+    }
+}
+
+let Kyle = Person.init()
+Kyle.eat()
+Kyle.hear()
+Kyle.feel()
+
+*/
+
+class People {
+    
+    var name = "사아람"
+    var age = 20
+    
+    func selfIntroduce() {
+        print("저는 \(age)살 \(name)입니다")
+    }
+    
+    init() {
+        
+    }
+}
+
+let chanwoo = People.init()
+chanwoo.name = "Kyle"
+chanwoo.age = 23
+chanwoo.selfIntroduce()
+
